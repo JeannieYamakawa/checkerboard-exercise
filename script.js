130f4c9a6ec0a33fb5017e8b22506695f5051cfd
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-08-24 18:36:00 -0500

    solution to random color checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// Your JS goes here
+// Each tile should be a div
+// Each tile's width is 11.1%
+// Set each tile's float property to left
+// Each tile's paddingBottom is 11.1%
+
+function createTile(color) {
+
+    var newDiv = document.createElement( "div" );
+
+    newDiv.style.setProperty("width", "11.1%");
+    newDiv.style.setProperty("background-color", color);
+    newDiv.style.setProperty("padding-bottom","11.1%");
+    newDiv.style.setProperty("float","left");
+    document.body.appendChild(newDiv);
+
+}
+
+var width = 9;
+var length = 9;
+
+var numOfTiles = width * length;
+
+function createBoard (){
+    var color = ''
+    for (var i =0; i<numOfTiles; i++){
+            var randomRed = Math.floor(Math.random()*255);
+            var randomGreen = Math.floor(Math.random()*255);
+            var randomBlue = Math.floor(Math.random()*255);
+            var colorString = "rgb("+ randomRed + ","+ randomGreen + "," + randomBlue+ ")";
+            color = colorString
+            createTile(color)
+        }
+}
+
+createBoard()
+
+
+//"rgb(255, 0, 0)""
